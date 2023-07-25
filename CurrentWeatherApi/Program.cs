3175a6e58cb3c2de964201686d593fbe6836d58c
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2023-07-24 20:38:58 -0400

    Inject http client to OpenWeatherService & set user secrets

--- CurrentWeatherApi/Program.cs
+++ CurrentWeatherApi/Program.cs
@@ -1,7 +1,14 @@
+using CurrentWeatherApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddHttpClient<IOpenWeatherService, OpenWeatherService>(client => 
+{
+    client.BaseAddress = new Uri(builder.Configuration["OpenWeatherBaseUrl"]);
+});
 
 var app = builder.Build();
+var appId = builder.Configuration["OpenWeatherId"];
 
 app.Run();
