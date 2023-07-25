20ac7ffbb3a3f73076b7685f2b24c1d549e03b3a
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2023-07-25 18:08:06 -0400

    Fix a wind spelling & display its unit of measurement

--- current-weather-client/src/components/CurrentWeather.js
+++ current-weather-client/src/components/CurrentWeather.js
@@ -48,8 +48,8 @@ function CurrentWeather({ isLoading, error, weather}){
       </div>
 
       <div className="bg-blue-100 text-blue-800 text-xs font-medium mr-2 mt-3 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
-        <label className="font-bold mr-2">Win Speed:</label>
-        { weather.windSpeed} %
+        <label className="font-bold mr-2">Wind Speed:</label>
+        { weather.windSpeed} miles/hour
       </div>
     </div>
     );
