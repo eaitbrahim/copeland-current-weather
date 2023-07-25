5a7b5d568a97215d385b7c86ab9e29411b892ee4
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2023-07-25 16:32:06 -0400

    Refactor weather search form & its state

--- current-weather-client/src/components/App.js
+++ current-weather-client/src/components/App.js
@@ -9,39 +9,41 @@ function App() {
   const [ isLoading, setIsLoading ] = useState(false);
   const [ error, setError ] = useState('');
   const [ searchOption, setSearchOption ] = useState('City');
-  const [ city, setCity ] = useState('Toledo,OH,US');
-  const [ zip, setZip ] = useState('43551');
-  const [ lat, setLat ] = useState(41.65424619050698);
-  const [ lon, setLon ] = useState(-83.53740534314433);
+  const [ location, setLocation ] = useState('');
 
   const apiUrl = 'http://localhost:5277/api/v1/';
 
   const onSubmit = (event) => {
     event.preventDefault();
     let endPoint;
+
     switch(searchOption){
       case 'City':
-        endPoint = `currentWeatherByCity/${city}`;
+        endPoint = `currentWeatherByCity/${location}`;
         break;
       case 'Zip':
-        endPoint = `currentWeatherByZip/${zip}`;
+        endPoint = `currentWeatherByZip/${location}`;
         break;
       case 'Coord':
       default:
-        endPoint = `currentWeatherByCoord/${lat}/${lon}`;
+        endPoint = `currentWeatherByCoord/${location.split(',')[0].trim()}/${location.split(',')[1].trim()}`;
     }
-    
+
+    setError('');
     setIsLoading(true);
     axios.get(apiUrl + endPoint)
           .then(res => {
             setIsLoading(false);
             setWeather(res.data);
-            setError('');
           })
           .catch(err => {
             setIsLoading(false);
-            setError('');
+            setError(`Failed to fetch current weather for the given location: ${location} & the error message is ${err.message}`);
             setWeather({});
+          })
+          .finally(() => {
+            setLocation('');
+            setSearchOption('City');
           });
   }
   return (
@@ -50,9 +52,8 @@ function App() {
         <h2 className="text-center text-blue-400 font-bold text-2xl uppercase mb-10">
           Current weather by location
         </h2>
-     
+    
         <div className="bg-white p-10 rounded-lg shadow md:w-3/4 mx-auto lg:w-1/2">
-
           <form onSubmit={onSubmit}>
               <label className="block mb-2 font-bold text-gray-600">Search Current Weather By</label>
               
@@ -73,34 +74,15 @@ function App() {
                 </span>
               </div>
 
-              { searchOption === 'City'  && (
-                  <div className="mb-5">
-                    <input className="border border-red-300 shadow p-3 w-full rounded mb-" value={city} onChange={(e) => setCity(e.target.value)} /> 
-                  </div>
-                )}
-                
-                { searchOption === 'Zip'  && (
-                  <div className="mb-5">
-                    <input className="border border-red-300 shadow p-3 w-full rounded mb-"  value={zip} onChange={(e) => setZip(e.target.value)} /> 
-                  </div>
-                )}
-
-                { searchOption === 'Coord'  &&
-                  (
-                    <div className="mb-5">
-                      <span>
-                        <input className="border border-red-300 shadow p-3 w-full rounded mb-"
-                          value={lat} onChange={(e) => setLat(Number(e.target.value))} />
-                        <input className="border border-red-300 shadow p-3 w-full rounded mb-"
-                          value={lon} onChange={(e) => setLon(Number(e.target.value))} />
-                      </span>
-                    </div>
-                  )}
               
-                <button type="submit" className={classNames("block w-full bg-blue-500 text-white font-bold p-4 rounded-lg", { "opaticty:80": isLoading})} 
-                  disabled={isLoading}>
-                    { isLoading ? <GoSync className="animate-spin"/> : 'Search' }
-                  </button>
+              <div className="mb-5">
+                <input className="border border-black-300 shadow p-3 w-full rounded" placeholder="Toledo or 43551 0r 41.65424619050698, -83.53740534314433" value={location} onChange={(e) => setLocation(e.target.value)} /> 
+              </div>
+              
+              <button type="submit" className={classNames("block w-full bg-blue-500 text-white font-bold p-4 rounded-lg", { "opaticty:80": isLoading})} 
+                disabled={isLoading | location === ''}>
+                  { isLoading ? <GoSync className="animate-spin"/> : 'Search' }
+              </button>
           </form>
 
           <hr className="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700">
--- current-weather-client/src/components/CurrentWeather.js
+++ current-weather-client/src/components/CurrentWeather.js
@@ -6,44 +6,54 @@ function CurrentWeather({ isLoading, error, weather}){
   }
 
   if(error){
-    return <div>Error fetching current weather...</div>;
+    return <div className="text-sm text-red-400 mt-2">{error}...</div>;
   }
 
-  return (
+  if(JSON.stringify(weather) !== '{}'){
+    return (
     <div className="mb-2 boarder rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
+      <div className="bg-blue-100 text-blue-800 text-xs font-medium mr-2 mt-3 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
+        <label className="font-bold mr-2">Date & Time:</label>
         { weather.timeDataCalculation}
       </div>
 
-      <div className="flex p-2 justify-between items-center cursor-pointer">
+      <div className="bg-blue-100 text-blue-800 text-xs font-medium mr-2 mt-3 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
+        <label className="font-bold mr-2">City:</label>
         { weather.name}
       </div>
 
-      <div className="flex p-2 justify-between items-center cursor-pointer">
+      <div className="bg-blue-100 text-blue-800 text-xs font-medium mr-2 mt-3 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
+        <label className="font-bold mr-2">Temperature:</label>
         { weather.temp} Fahrenheit
       </div>
 
-      <div className="flex p-2 justify-between items-center cursor-pointer">
+      <div className="bg-blue-100 text-blue-800 text-xs font-medium mr-2 mt-3 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
+        <label className="font-bold mr-2">Feels like:</label>
         { weather.feelsLike} Fahrenheit
       </div>
 
-      <div className="flex p-2 justify-between items-center cursor-pointer">
+      <div className="bg-blue-100 text-blue-800 text-xs font-medium mr-2 mt-3 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
+        <label className="font-bold mr-2">Description:</label>
         { weather.description}
       </div>
 
-      <div className="flex p-2 justify-between items-center cursor-pointer">
+      <div className="bg-blue-100 text-blue-800 text-xs font-medium mr-2 mt-3 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
+        <label className="font-bold mr-2">Pressure:</label>
         { weather.pressure} hPa
       </div>
 
-      <div className="flex p-2 justify-between items-center cursor-pointer">
+      <div className="bg-blue-100 text-blue-800 text-xs font-medium mr-2 mt-3 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
+        <label className="font-bold mr-2">Humidity:</label>
         { weather.humidity} %
       </div>
 
-      <div className="flex p-2 justify-between items-center cursor-pointer">
+      <div className="bg-blue-100 text-blue-800 text-xs font-medium mr-2 mt-3 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
+        <label className="font-bold mr-2">Win Speed:</label>
         { weather.windSpeed} %
       </div>
     </div>
     );
+  }
 }
 
 export default CurrentWeather;
